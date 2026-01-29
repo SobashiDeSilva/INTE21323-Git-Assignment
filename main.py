1d42a833de2453bbd28e513d7b28bb03317f4cc4
SobashiDeSilva <desilvasobashi.23@gmail.com>
2026-01-29 14:57:14 +0530

    Initial commit: Added simple addition script and readme

---
+++ main.py
@@ -0,0 +1,6 @@
+# Simple addition function for INTE 21323
+def add_numbers(num1, num2):
+    return num1 + num2
+
+if __name__ == "__main__":
+    print("The sum of 10 and 20 is:", add_numbers(10, 20))
---
+++ readme.md
@@ -0,0 +1,2 @@
+# GitHub Workflow Mastery Challenge
+This project contains a simple Python script to show addition of two numbers.
